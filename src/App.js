b736ee79f9df32e08687774a74fc0f3767741e09
jnanendra07 <jnanendra2003@gmail.com>
2023-10-14 22:36:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Calculator from './Calculator';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>&nbsp;</h1>
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
